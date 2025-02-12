0a16cb22c5e4755965b66b9057c276b4b4a5a074
Chloé Morali <cmorali@ymail.com>
2025-02-12 13:13:38 -0800

    About edits

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -8,9 +8,8 @@ const About = () => {
       <p className='uppercase text-xl tracking-widest text-[#9d174d] text-center mb-12'>About Me</p>
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8 p-8'>
         <div className='col-span-2'>
-          <p className='py-2 text-stone-600'>I&apos;m seeking a challening engineering position at a global tech company that differentiates itself through innovation, customer delight, and talent development.</p>
-          <p className='py-2 text-stone-600'>I recently graduated from Santa Clara University with a degree in Computer Science & Engineering. I also had the opportunity to study at Yonsei University in Seoul in Fall 2022 and at L&apos;Institut Français de la Mode in Paris in Summer 2023.</p>
-          <p className='py-2 text-stone-600'> Over the past year, during my Senior Design Project, my teammate and I designed an architecture to manage and secure IoT devices on small and vulnerable networks. We productized it into a mobile app to empower users to confidently embrace the benefits of smart devices.</p>
+          <p className='py-2 text-stone-600'>I&apos;m seeking a challening engineering position at a global tech company that differentiates itself through innovation, customer delight, and talent development. I'm interested in a rotational program opportunity to grow and explore.</p>
+          <p className='py-2 text-stone-600'>I recently graduated from Santa Clara University with a degree in Computer Science & Engineering. For my Senior Design Project, my teammate and I designed a system to manage and secure IoT devices on vulnerable networks. We productized it into a mobile app to empower users to embrace smart devices without fear.</p>
           <p className='py-2 text-stone-600'>I can&apos;t wait to bring my commitment to excellence and innovation to real-world solutions!</p>
         </div>
         <div className='w-full h-auto m-auto flex items-center justify-center'>
