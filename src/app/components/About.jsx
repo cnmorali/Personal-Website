a26c00646690a14bc60307280bda04af6042ab41
Chloé Morali <cmorali@ymail.com>
2025-02-12 13:22:26 -0800

    About edits

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <p className='uppercase text-xl tracking-widest text-[#9d174d] text-center mb-12'>About Me</p>
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8 p-8'>
         <div className='col-span-2'>
-          <p className='py-2 text-stone-600'>I&apos;m seeking a challening engineering position at a global tech company that differentiates itself through innovation, customer delight, and talent development. I'm interested in a rotational program opportunity to grow and explore.</p>
+          <p className='py-2 text-stone-600'>I&apos;m seeking a challening engineering position at a global tech company that differentiates itself through innovation, customer delight, and talent development. I&apos;m interested in a rotational program opportunity to grow and explore.</p>
           <p className='py-2 text-stone-600'>I recently graduated from Santa Clara University with a degree in Computer Science & Engineering. For my Senior Design Project, my teammate and I designed a system to manage and secure IoT devices on vulnerable networks. We productized it into a mobile app to empower users to embrace smart devices without fear.</p>
           <p className='py-2 text-stone-600'>I can&apos;t wait to bring my commitment to excellence and innovation to real-world solutions!</p>
         </div>
